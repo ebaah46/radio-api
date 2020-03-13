8b691e54f656c9e99d5d1a1a0caa2537fe34e2a5
ebaah46 <ebaah46@yahoo.com>
2020-03-13 23:41:05 +0000

    heroku fix

--- app/Http/Controllers/MessageApiController.php
+++ app/Http/Controllers/MessageApiController.php
@@ -21,10 +21,7 @@ class MessageApiController extends Controller
 //        $audio_files =[];
 //        $picture_files =[];
         try{
-//            $messages = Message::all();
-            $messages = 'test route';
-            echo $messages;
-            exit();
+            $messages = Message::all();
             return response()->json([
                 'status_code'=>200,
                 'data'=>$messages,
